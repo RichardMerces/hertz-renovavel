d856cff0bb31a112444a20c1e48d66c025b9d656
francieleber <francielebernardino545@gmail.com>
2022-11-30 12:30:07 -0300

    atualizando o sobre

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import './Navbar.css'
 import AppBar from '@material-ui/core/AppBar';
-import { Button, Toolbar, Typography } from "@material-ui/core";
+import { Button, FormControl, Input, InputAdornment, InputLabel, Toolbar, Typography } from "@material-ui/core";
+import SearchIcon  from "@material-ui/icons/Search";
 
 
 function Navbar() {
@@ -14,7 +15,19 @@ function Navbar() {
           </Typography>
           <Button color="inherit">Sobre</Button>
           <Button color="inherit">Login</Button>
+          <FormControl>
+        <InputLabel></InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon/>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
         </Toolbar>
+
       </AppBar>
     </>
   );
--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -5,8 +5,14 @@ function Sobre(){
     return (
         <>
             <h1>
-                Sobre nós.
-            </h1>
+                Sobre nós
+                </h1> <p>
+                A  HERTZ é uma empresa de engenharia elétrica que oferece diversas estratégias com base na gestão inteligente da energia elétrica, atuando com responsabilidade e ética profissional e empresarial.
+
+O corpo técnico da empresa é composto por eletricistas, eletrotécnicos e engenheiros, que asseguram conformidade técnica, qualidade e o menor custo de implementação.
+
+A empresa presta assessoria durante todas as etapas de negociação, sendo uma parceira dos clientes que buscam uma relação de confiança e fidelidade.
+            </p>
         </>
     );
 }
