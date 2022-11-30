5752fd5d8d1af75279f64110d9040aafb2991de2
francieleber <francielebernardino545@gmail.com>
2022-11-30 11:39:06 -0300

    modificando a home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,18 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import './Home.css'
 
 function Home(){
     return (
-        <>
+        <Grid container direction='row' justifyContent='center' alignItems='center'> 
             <h1>Olá, Mundo!</h1>
-        </>
+
+            <Grid xs={12} className='imagem'>
+                
+            </Grid>
+               
+            
+        </Grid>
     );
 }
 
