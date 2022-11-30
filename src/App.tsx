271a742ba59c0719ad20e0f5d85cbcfbff6e89d4
francieleber <francielebernardino545@gmail.com>
2022-11-30 10:23:27 -0300

    Navbar criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/estaticos/navbar/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route></Route>
       </Routes>
---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Navbar.css'
+import AppBar from '@material-ui/core/AppBar';
+import { Button, Toolbar, Typography } from "@material-ui/core";
+
+
+function Navbar(){
+    <>
+    <AppBar position="static">
+  <Toolbar>
+    <Typography variant="h6">
+      HERTZ
+    </Typography>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
+    
+    </>
+}
+export default Navbar;
