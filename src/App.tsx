3358a6a20dd8147fa978f393e9e6e8866a0eb8ee
unknown <thayjsilva@gmail.com>
2022-11-30 10:53:56 -0300

    adicionando o footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/estaticos/footer/Footer';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Home from './pages/home/Home';
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
