02ab29ed34f16a88fbd6dc9c6f8f2ce478796a86
RichardMerces <richardmerces@outlook.com>
2022-12-08 10:46:22 -0300

    resolvendo a navbar

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -2,23 +2,51 @@ import React from "react";
 import './Navbar.css'
 import AppBar from '@material-ui/core/AppBar';
 import { Button, FormControl, Input, InputAdornment, InputLabel, Toolbar, Typography } from "@material-ui/core";
-import SearchIcon from "@material-ui/icons/Search";
 import { Box } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
 
 
 function Navbar() {
+
+  const [token, setToken] = useLocalStorage('token');
+  const navigate = useNavigate();
+
+  function goLogout() {
+    setToken('')
+    navigate("/login")
+  }
+
   return (
     <>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar variant="dense">
           <Typography variant="h6">
             HERTZ
           </Typography>
-          <Button color="inherit">Home</Button>
-          <Button color="inherit">Sobre</Button>
-          <Button color="inherit">Logout</Button>
-          <Box display='flex' justifyContent='end'>
-          </Box>
+          <Box display="flex" justifyContent="start">
+                        <Link to="/home" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Home
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to="/sobre" className="text-decorator-none">
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Sobre
+                                </Typography>
+                            </Box>
+                        </Link>
+                        
+                            <Box mx={1} className='cursor' onClick={goLogout}>
+                                <Typography variant="h6" color="inherit">
+                                    logout
+                                </Typography>
+                            </Box>
+                        
+                    </Box>
         </Toolbar>
 
       </AppBar>
