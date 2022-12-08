ac5872f2c7b150f4e2c21d9c0409eab8de64f881
RichardMerces <richardmerces@outlook.com>
2022-12-08 10:59:19 -0300

    criando lista produtos, lista categorias e criando models

---
+++ src/models/Categoria.ts
@@ -0,0 +1,8 @@
+interface Categoria {
+    idCategoria: number;
+    nome: string;
+    tipo: string;
+    descricao: string
+}
+
+export default Categoria;
---
+++ src/models/Produto.ts
@@ -0,0 +1,9 @@
+interface Produto {
+    idProduto: number;
+    nome: string;
+    descricao: string;
+    preco: number;
+    foto_produto: string
+}
+
+export default Produto;
