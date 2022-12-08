58a4d3a583ef3f3e1a695a6cabf6d443708a4f55
francieleber <francielebernardino545@gmail.com>
2022-12-08 11:54:29 -0300

    ListaProduto criada

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/estaticos/footer/Footer';
 import Navbar from './components/estaticos/navbar/Navbar';
+import ListaProduto from './components/produtos/listaProdutos/ListaProdutos';
 import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
@@ -23,6 +24,9 @@ function App() {
         <Route path="/sobre" element={<Sobre />} />
 
         <Route path="/cadastrar" element={<CadastroUsuario />} />
+        
+        <Route path="/produto" element={<ListaProduto />} />
+
 
       </Routes>
       <Footer />
---
+++ src/components/produtos/listaProdutos/ListaProdutos.tsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import {Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import {Box} from '@mui/material';
+import './ListaProdutos.css';
+import useLocalStorage from 'react-use-localstorage';
+import {useNavigate, useParams } from 'react-router-dom'
+import Produto from '../../../models/Produto';
+
+function ListaProduto() {
+  const [produto, setProduto] = useState<Produto[]>([])
+  const [token, setToken] = useLocalStorage('token');
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (token == "") {
+      alert("Você precisa estar logado")
+      navigate("/login")
+
+    }
+  }, [token])
+
+  async function getProduto() {
+    await busca("/produtos", setProduto, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+  useEffect(() => {
+
+    getProduto()
+
+  }, [produto.length])
+
+  return (
+    <>
+      {
+        produto.map(produto => (
+          <Box m={2} >
+            <Card variant="outlined">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Postagens
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {produto.nome}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {produto.idProduto}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {produto.foto_produto}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box display="flex" justifyContent="center" mb={1.5}>
+
+                  <Link to={`/formularioProduto/${produto.idProduto}`} className="text-decorator-none" >
+                    <Box mx={1}>
+                      <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                        atualizar
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link to={`/deletarProduto/${produto.idProduto}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" size='small' color="secondary">
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
+                </Box>
+              </CardActions>
+            </Card>
+          </Box>
+        ))
+      }
+    </>
+  )
+}
+
+export default ListaProduto;
+
+function busca(arg0: string,setProdutos: React.Dispatch<React.SetStateAction<Produto[]>>, arg2: { headers: { Authorization: string; }; }) {
+    throw new Error('Function not implemented.');
+}
